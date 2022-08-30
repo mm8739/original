64d4eea524a9ae6dd384ac915f2182749471e954
Misaki Mori <misaki.h.0928@gmail.com>
2022-08-30 17:20:09 +0900

    Release to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+resources :users
   get 'hello/index' => 'hello#index'
-  root 'hello#index'
+  root 'tweets#index'
 
   get 'hello/link' => 'hello#link'
 
     resources :tweets
   
-  resources :users
+  
 end
